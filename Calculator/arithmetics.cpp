6dfd0576ea9fec7f3b7c0d01a22b35c649ccf7ca
Chimunya Masenke <chimunyamasenke@gmail.com>
2026-03-24 00:15:20 +0200

    Removed arithmetic modules from main.cpp and add Factorial function. //TODO FIX INPUT FOR FACTORIAL

---
+++ Calculator/arithmetics.cpp
@@ -0,0 +1,26 @@
+#include "calc.h"
+
+double Addition(double num1, double num2) {
+  double sum = num1 + num2;
+  return sum;
+}
+
+double Substraction(double num1, double num2) {
+  double difference = num1 - num2;
+  return difference;
+}
+
+double Multiplication(double num1, double num2) {
+  double product = num1 * num2;
+  return product;
+}
+
+double Division(double num1, double num2) {
+  double quotient = num1 / num2;
+  return quotient;
+}
+
+int Factorial(int num){
+    if(num <= 1) return num;
+    return num * Factorial(num - 1);
+}
---
+++ Calculator/calc.h
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by Nchimunya Masenke on 12/09/2025.
+//
+
+#include <iostream>
+using namespace std;
+
+double Addition(double, double);
+double Substraction(double, double);
+double Multiplication(double, double);
+double Division(double, double);
+int Factorial(int);
